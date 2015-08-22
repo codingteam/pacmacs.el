7a713ac0b7c80ea8a39af1bf747fc8a40e895d89
rexim <reximkut@gmail.com>
2015-08-22 22:26:41 +0600

    Add UTs again

---
+++ test/pacman-test.el
@@ -0,0 +1,6 @@
+(ert-deftest pacman-anim-get-frame-test ()
+  (let ((anim (list :frames (number-sequence 1 5)
+                    :current-frame 2)))
+    (should (equal 3
+                   (pacman-anim-get-frame anim)
+                   ))))
