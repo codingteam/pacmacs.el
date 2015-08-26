b8d1136c3a329d2d7c831b530ef5392f39055a54
rexim <reximkut@gmail.com>
2015-08-26 22:53:34 +0600

    Extract pacman-anim module (#19)

--- pacman.el
+++ pacman.el
@@ -27,6 +27,8 @@
 ;; CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 ;; SOFTWARE.
 
+(require 'pacman-anim)
+
 (defconst pacman-buffer-name "*Pacman*")
 
 (defvar pacman-timer nil)
@@ -60,24 +62,6 @@
 
 (defun pacman-insert-image (resource resource-vector)
   (insert-image resource " " nil resource-vector))
-
-;;;###autoload
-(defun pacman-anim-get-frame (anim)
-  (let ((frames (plist-get anim :frames))
-        (current-frame (plist-get anim :current-frame)))
-    (nth current-frame frames)))
-
-(defun pacman-anim-next-frame (anim)
-  (let* ((frames (plist-get anim :frames))
-         (current-frame (plist-get anim :current-frame))
-         (new-current-frame (mod (+ current-frame 1)
-                                 (length frames))))
-    (plist-put anim :current-frame new-current-frame)))
-
-(defun pacman-anim-object-next-frame (anim-object)
-  (let ((anim (plist-get anim-object :animation)))
-    (plist-put anim-object :animation
-               (pacman-anim-next-frame anim))))
 
 (defun pacman-start ()
   (interactive)
--- test/pacman-anim-test.el
+++ test/pacman-anim-test.el
@@ -2,5 +2,4 @@
   (let ((anim (list :frames (number-sequence 1 5)
                     :current-frame 2)))
     (should (equal 3
-                   (pacman-anim-get-frame anim)
-                   ))))
+                   (pacman-anim-get-frame anim)))))
