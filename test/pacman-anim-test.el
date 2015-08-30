515b977a4c627832f8f66b54ffa0a35c3fa986b2
rexim <reximkut@gmail.com>
2015-08-30 16:11:21 +0600

    Add UT for pacman-anim-next-frame function (#22)

--- test/pacman-anim-test.el
+++ test/pacman-anim-test.el
@@ -1,5 +1,13 @@
 (ert-deftest pacman-anim-get-frame-test ()
   (let ((anim (list :frames (number-sequence 1 5)
                     :current-frame 2)))
-    (should (equal 3
-                   (pacman-anim-get-frame anim)))))
+    (should (equal 3 (pacman-anim-get-frame anim)))))
+
+(ert-deftest pacman-anim-next-frame-test ()
+  (let ((anim (list :frames (number-sequence 1 4)
+                    :current-frame 2)))
+    (pacman-anim-next-frame anim)
+    (should (equal 3 (plist-get anim :current-frame)))
+
+    (pacman-anim-next-frame anim)
+    (should (equal 0 (plist-get anim :current-frame)))))
