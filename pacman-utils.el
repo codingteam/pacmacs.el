c20aa8089ce7f396f225f581f75b8c8998363a87
rexim <reximkut@gmail.com>
2015-09-19 20:47:46 +0600

    Implement plist-bind macro. Close #47

---
+++ pacman-utils.el
@@ -0,0 +1,12 @@
+
+(defmacro plist-bind (keys expr &rest body)
+  (declare (indent 2) (debug t))
+  (let ((expr-name (gensym)))
+    `(let* ((,expr-name ,expr)
+            ,@(mapcar '(lambda (key)
+                         (cons (car key)
+                               `((plist-get ,expr-name ,(cadr key)))))
+                      keys))
+       ,@body)))
+
+(provide 'pacman-utils)
