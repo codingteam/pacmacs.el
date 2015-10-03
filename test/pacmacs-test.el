05c88e5eef27a556a3f93630972563bc6685002e
rexim <reximkut@gmail.com>
2015-10-04 02:40:24 +0600

    UTs for pacmacs--object-at-p (#75)

--- test/pacmacs-test.el
+++ test/pacmacs-test.el
@@ -18,3 +18,15 @@
                          [nil nil nil nil nil]]))
     (should (equal expected-board
                    (pacmacs--init-board width height)))))
+
+(ert-deftest pacmacs--object-at-p-test ()
+  (let ((pacmacs-board-width 5)
+        (pacmacs-board-height 4)
+        (objects (-map #'(lambda (x)
+                           (list :row x
+                                 :column x))
+                       (number-sequence 0 3))))
+    (should (pacmacs--object-at-p 0 0 objects))
+    (should (not (pacmacs--object-at-p 0 1 objects)))
+    (should (pacmacs--object-at-p 0 5 objects))
+    (should (not (pacmacs--object-at-p 1 5 objects)))))
