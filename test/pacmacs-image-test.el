codingteam/pacmacs.el

commit ce03d91967d33e1b7eda496ffcea2b58203f715b

Introduce xbm flip bit hack (#133)

pacmacs-image.el
@@ -32,6 +32,8 @@
 
 ;;; Code:
 
+(defconst pacmacs--flip-xbm-bits (eq system-type 'windows-nt))
+
 (defun pacmacs-load-image (filename)
   (create-image filename 'xpm nil :heuristic-mask t))
 
@@ -39,12 +41,18 @@
   (insert-image resource " " nil resource-vector))
 
 (defun pacmacs-create-color-block (width height color)
-  (create-image
+  (apply
+   #'create-image
    (make-vector
     width (make-bool-vector height t))
    'xbm t :width width :height height
-   :foreground color
-   :background color))
+   (if (not pacmacs--flip-xbm-bits)
+       (list
+        :foreground color
+        :background nil)
+     (list
+      :foreground nil
+      :background color))))
 
 (defun pacmacs-create-transparent-block (width height)
   (create-image

commit 98599015bb5eaf77bad881054da09b74409b148e

Fix UTs broken by #133 implementation

test/pacmacs-image-test.el
@@ -20,11 +20,19 @@
     (with-mock
      (mock (make-bool-vector height t) => bool-vector-result :times 1)
      (mock (make-vector width bool-vector-result) => make-vector-result :times 1)
-     (mock (create-image make-vector-result
-                         'xbm t
-                         :width width :height height
-                         :foreground color
-                         :background color) => create-image-result :times 1)
+
+     (if (not pacmacs--flip-xbm-bits)
+         (mock (create-image make-vector-result
+                             'xbm t
+                             :width width :height height
+                             :foreground color
+                             :background nil) => create-image-result :times 1)
+       (mock (create-image make-vector-result
+                           'xbm t
+                           :width width :height height
+                           :foreground nil
+                           :background color) => create-image-result :times 1))
+
      (should (equal create-image-result
                     (pacmacs-create-color-block width height color))))))
 