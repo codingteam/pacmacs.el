d3c94de6dace97eb6c9d4fda0c288c0b2655ad29
rexim <reximkut@gmail.com>
2015-11-23 00:36:20 +0600

    Remove UT for pacmacs--track-point (#159)

--- test/pacmacs-test.el
+++ test/pacmacs-test.el
@@ -7,20 +7,6 @@
     (should (not (pacmacs--cell-tracked-p 0 0)))
     (should (pacmacs--cell-tracked-p 1 0))))
 
-(ert-deftest pacmacs--track-point-test ()
-  (let ((pacmacs--track-board (list :width 2
-                                     :height 2
-                                     :data [[nil nil]
-                                            [nil nil]])))
-    (pacmacs--track-point (cons 0 0) (cons 0 1))
-    (should (equal [[right nil]
-                    [nil nil]]
-                   (plist-get pacmacs--track-board :data)))
-    (pacmacs--track-point (cons 1 0) (cons 1 -1))
-    (should (equal [[right nil]
-                    [left nil]]
-                   (plist-get pacmacs--track-board :data)))))
-
 (ert-deftest pacmacs--track-object-test ()
   (let ((pacmacs--track-board (list :width 2
                                      :height 2
